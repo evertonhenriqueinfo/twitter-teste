98f7e1fd4bfedd07a121cbe51b020faba01a2ba5
Everton <evertonhenrique.info@gmail.com>
2023-09-03 23:51:44 -0300

    original ProfileTest.php and fixed TwitterTest.php

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,10 +2,6 @@
 
 use App\Models\User;
 
-todo('should be able to create a tweet');
-todo('body is required');
-todo('the tweet body should have a max length of 140 characters');
-todo('should show the tweet on the timeline');
 
 test('profile page is displayed', function () {
     $user = User::factory()->create();
